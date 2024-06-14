9606863fe354bcd5ff0f23e5c4373767ba6d57c9
AzamatSV5 <tursunovazamat0823@gmail.com>
2024-06-14 21:56:05 +0500

    first update

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-let coffee = document.querySelector('.img')
-let count = document.querySelector('.counter')
-let i = 0
+// let coffee = document.querySelector('.img')
+// let count = document.querySelector('.counter')
+// let i = 0
 
 
 
-let clickMug = () => {
-    count.innerHTML = ++i
+// let clickMug = () => {
+//     let i = 0
+//     count.innerHTML = ++i
 
 
-}
+// }
+
+// setTimeout(() => {
+//     coffee.style.transform = 'scale(1)';
+// }, 300);
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let coffee = document.querySelector('.img')
+    let count = document.querySelector('.counter')
+    let i = 0
+
+
+
+
+
+
+    coffee.addEventListener('mousedown', () => {
+
+
+        coffee.style.transform = 'scale(1.2)';
+    });
+
+    coffee.addEventListener('mouseup', () => {
+        coffee.style.transform = 'scale(1)';
+        count.innerHTML = ++i
+    });
+
+    coffee.addEventListener('mouseleave', () => {
+        coffee.style.transform = 'scale(1)';
+    });
+})
